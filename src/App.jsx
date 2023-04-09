ffc3f7232f3d14f4d40c07daf2ff9af1df3a5464
Victor Delgado <valexdelgado@gmail.com>
2023-04-08 23:02:00 -0700

    finished lights out project (looked at the solution quite a bit...)

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Board from './Board'
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Board />
+    </div>
+  )
+}
+
+export default App
